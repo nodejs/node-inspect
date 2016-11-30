3e1a66a489bef19beaa5f859e99e027274ff43cb
Jan Krems <jan.krems@groupon.com>
2016-11-30 12:04:45 -0600

    feat: Support CPU & heap profiles

--- test/cli/profile.test.js
+++ test/cli/profile.test.js
@@ -3,6 +3,10 @@ const { test } = require('tap');
 
 const startCLI = require('./start-cli');
 
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 test('profiles', (t) => {
   const cli = startCLI(['examples/empty.js']);
 
@@ -18,8 +22,10 @@ test('profiles', (t) => {
       t.match(cli.output, 'undefined');
     })
     .then(() => cli.command('exec console.profileEnd()'))
+    .then(() => delay(250))
     .then(() => {
       t.match(cli.output, 'undefined');
+      t.match(cli.output, 'Captured new CPU profile.');
     })
     .then(() => cli.quit())
     .then(null, onFatal);
